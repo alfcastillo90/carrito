e7b0a441a5522d4526418c3738a72017182fb42f
alfcastillo90 <cacr1990@gmail.com>
2017-07-25 04:55:52 -0400

    se hicieron cambios en el archivo logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,24 @@
 <?php
-    session_start(); //mantiene activa la sesion
-    session_destroy(); //destruye la sesion iniciada
-    header('Location: ./index.html'); //posicionamos la cabecera
-    exit(0); //salida
+//Reanudamos la sesión
+session_start();
+//Requerimos los datos de la conexión a la BBDD
+require('conexion.php');
+//Des-establecemos todas las sesiones
+unset($_SESSION);
+//Destruimos las sesiones
+session_destroy();
+//Cerramos la conexión con la base de datos
+mysqli_close($conexion);
+//Redireccionamos a el index
+header("Location: ../index.php");
+die();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cerrando sesión...</title>
+</head>
+<body>
+</body>
+</html
